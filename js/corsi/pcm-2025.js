ce87a44cc4f7d8525814e862c494252c8ab95f40
Massimo Gobbino <massimo.gobbino@unipi.it>
2026-07-25 17:35:54 +0200

    Add Precorso per Matematica 2025

---
+++ js/corsi/pcm-2025.js
@@ -0,0 +1,45 @@
+var NomeCorso = "Precorso per Matematica - 2025";
+
+var Percorso = MEDIA.lezioni + "Precorsi/PCM_25/PCM_25_L";
+
+var CifreNumeroLezione = 2;
+
+var SchedaCorso = {
+  nome: "Precorso per Matematica",
+  annoAccademico: "2025/2026",
+  crediti: "Nessuno",
+  ore: "20",
+  target: "Corso di Laurea in Matematica",
+  anno: "Prima dell’inizio del primo anno",
+  periodo: "15–19 settembre 2025"
+};
+
+var DocumentiCorso = [
+  {
+    sezione: "book",
+    file: "PCM_25_Book.pdf",
+    titolo: "Stampato integrale delle lezioni"
+  }
+];
+
+var Lezioni = new Array();
+Lezioni[1] = ["1", "15/09/2025", "14:00", "15:00", "Numeri interi: fattorizzazione, divisibilità, massimo comun divisore e minimo comune multiplo. Polinomi. Triangolo di Tartaglia."];
+Lezioni[2] = ["2", "15/09/2025", "15:00", "16:00", "Teorema di Ruffini. Divisione di polinomi. Fattorizzazioni notevoli."];
+Lezioni[3] = ["3", "15/09/2025", "16:00", "17:00", "Equazioni di primo e secondo grado. Equazioni riconducibili al secondo grado mediante cambi di variabile. Ricerca di radici razionali per polinomi. Equazioni polinomiali che si risolvono mediante fattorizzazione."];
+Lezioni[4] = ["4", "15/09/2025", "17:00", "18:00", "Disequazioni di primo grado. Disequazioni di secondo grado: interpretazione in termini di fattorizzazione e grafico. Sistemi di disequazioni."];
+Lezioni[5] = ["5", "16/09/2025", "14:00", "15:00", "Radicali e potenze frazionarie."];
+Lezioni[6] = ["6", "16/09/2025", "15:00", "16:00", "Studio del segno di prodotti e quozienti sulla base del segno dei singoli fattori."];
+Lezioni[7] = ["7", "16/09/2025", "16:00", "17:00", "Disequazioni fratte."];
+Lezioni[8] = ["8", "16/09/2025", "17:00", "18:00", "Valore assoluto: definizione. Equazioni e disequazioni elementari con il valore assoluto. Ulteriori esempi di disequazioni che si riducono a prodotti e quozienti."];
+Lezioni[9] = ["9", "17/09/2025", "14:00", "15:00", "Logaritmi: definizione operativa e principali proprietà. Esercizi sui logaritmi."];
+Lezioni[10] = ["10", "17/09/2025", "15:00", "16:00", "Ulteriori esercizi sulle potenze. Equazioni con esponenziali e logaritmi."];
+Lezioni[11] = ["11", "17/09/2025", "16:00", "17:00", "Disequazioni con esponenziali e logaritmi."];
+Lezioni[12] = ["12", "17/09/2025", "17:00", "18:00", "Equazioni e disequazioni con radici e valori assoluti."];
+Lezioni[13] = ["13", "18/09/2025", "14:00", "15:00", "Introduzione alla trigonometria: angoli e archi, gradi e radianti, funzioni trigonometriche, archi e angoli notevoli, archi associati."];
+Lezioni[14] = ["14", "18/09/2025", "15:00", "16:00", "Formule trigonometriche classiche: addizione e sottrazione, duplicazione, formule di prostaferesi e di Werner."];
+Lezioni[15] = ["15", "18/09/2025", "16:00", "17:00", "Equazioni e disequazioni trigonometriche."];
+Lezioni[16] = ["16", "18/09/2025", "17:00", "18:00", "Relazioni trigonometriche nei triangoli rettangoli. Introduzione alla risoluzione dei triangoli generali: teorema dei seni, teorema del coseno e formula trigonometrica per l’area. Discussione dei tre scenari possibili."];
+Lezioni[17] = ["17", "19/09/2025", "14:00", "15:00", "Equazione della retta nel piano cartesiano: significato geometrico dei coefficienti, rette parallele e perpendicolari, distanza di un punto da una retta. Asse di un segmento e bisettrici di un angolo."];
+Lezioni[18] = ["18", "19/09/2025", "15:00", "16:00", "Equazione della circonferenza nel piano cartesiano. Significato geometrico dei coefficienti. Retta tangente in un punto. Condizione di tangenza tra una retta e una circonferenza."];
+Lezioni[19] = ["19", "19/09/2025", "16:00", "17:00", "Disegno di sottoinsiemi del piano descritti da sistemi di disequazioni."];
+Lezioni[20] = ["20", "19/09/2025", "17:00", "18:00", "Esercizi su parabole, ellissi e iperboli in posizione canonica. Angoli al centro e alla circonferenza. Dimostrazione del teorema dei seni."];
