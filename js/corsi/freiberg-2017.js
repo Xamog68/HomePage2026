5667e7752b17b7df49146d35354f094d5c40bb98
Massimo Gobbino <massimo.gobbino@unipi.it>
2026-07-26 17:27:12 +0200

    Migra il corso di dottorato di Freiberg

---
+++ js/corsi/freiberg-2017.js
@@ -0,0 +1,32 @@
+var NomeCorso = "Freiberg 2017";
+var Percorso = MEDIA.lezioni + "Freiberg_17/Freiberg_17_L";
+var CifreNumeroLezione = 1;
+
+var SchedaCorso = {
+  nome: "Linear hyperbolic equations with time-dependent propagation speed and strong damping",
+  annoAccademico: "",
+  crediti: "",
+  ore: "8",
+  target: "PhD students in Mathematics",
+  anno: "",
+  periodo: "Freiberg (Germany), May 2017",
+  avviso: ""
+};
+
+var DocumentiCorso = [
+  {
+    sezione: "book",
+    file: "../../../Tablet/Freiberg_17/Freiberg_17_Book.pdf",
+    titolo: "Printout of lectures"
+  }
+];
+
+var Lezioni = new Array();
+Lezioni[1] = [ "1", "22/05/2017", "16:00", "17:00", "Preliminaries and functional spaces: Hilbert spaces, multiplication operators, Sobolev spaces, distributions, Gevrey spaces and Gevrey ultradistributions. Continuity moduli."];
+Lezioni[2] = [ "2", "22/05/2017", "17:00", "18:00", "Approximated energy estimates for ODEs. Kovaleskian energy and hyperbolic energy. Classical applications: integrable coefficients, Lipschitz coefficients, omega-continuous coefficients."];
+Lezioni[3] = [ "3", "23/05/2017", "14:00", "15:00", "Classical well-posedness theory for equations with time-dependent propagation speed. Higher space-regularity of data compensates lower time-regularity of the propagation speed."];
+Lezioni[4] = [ "4", "23/05/2017", "15:00", "16:00", "Equations with constant propagation speed and strong damping: well-posedness results (with a full description of possible phase spaces) and higher regularity for positive times."];
+Lezioni[5] = [ "5", "24/05/2017", "09:00", "10:00", "Equations with time-dependent propagation speed and strong damping: well-posedness in the supercritical case. Heuristics and proof through an adapted Kovaleskian energy."];
+Lezioni[6] = [ "6", "24/05/2017", "10:00", "11:00", "Equations with time-dependent propagation speed and strong damping: well-posedness in the subcritical case. Heuristics and proof through an adapted hyperbolic energy."];
+Lezioni[7] = [ "7", "26/05/2017", "11:00", "12:00", "ODEs with time-dependent propagation speed (with or without dissipation): examples of solutions whose energy grows exponentially."];
+Lezioni[8] = [ "8", "26/05/2017", "12:00", "13:00", "Equations with time-dependent propagation speed: how to build counterexamples showing the optimality of the well-posedness results."];
