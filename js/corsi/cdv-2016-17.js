91f84c46737f51e8e0d9beedef7a06ff7353ce42
Massimo Gobbino <massimo.gobbino@unipi.it>
2026-07-26 16:24:54 +0200

    Migra e organizza i corsi di Calcolo delle variazioni

---
+++ js/corsi/cdv-2016-17.js
@@ -0,0 +1,92 @@
+var NomeCorso = "Elementi di Calcolo delle Variazioni - 2016/2017";
+var Percorso = MEDIA.lezioni + "CdV_17/CdV_17_L";
+var CifreNumeroLezione = 2;
+
+var SchedaCorso = {
+  nome: "Elementi di Calcolo delle Variazioni",
+  annoAccademico: "2016/2017",
+  crediti: "6",
+  ore: "48",
+  target: "Students in Mathematics",
+  anno: "Third year students or more",
+  periodo: "End of September / December",
+  avviso: "This course is an English version of the course held in Italian in 2015/2016, hopefully better organized and improved by the experience gained in the previous year."
+};
+
+var DocumentiCorso = [
+  {
+    sezione: "burocrazia",
+    file: "CdV_17_Ufficiale.pdf",
+    titolo: "Official presentation"
+  },
+  {
+    sezione: "burocrazia",
+    file: "CdV_17_Burocrazia.pdf",
+    titolo: "General informations"
+  },
+  {
+    sezione: "burocrazia",
+    file: "CdV_17_Esame.pdf",
+    titolo: "Exam rules"
+  },
+  {
+    sezione: "burocrazia",
+    file: "CdV_17_Programma.pdf",
+    titolo: "Detailed program"
+  },
+  {
+    sezione: "book",
+    file: "CdV_17_Book.pdf",
+    titolo: "Printout of lectures"
+  }
+];
+
+var Lezioni = new Array();
+Lezioni[1] = [ "1", "28/09/2016", "16:00", "17:00", "Presentation of the main topics of the course through simple examples. Integral functionals in different settings."];
+Lezioni[2] = [ "2", "28/09/2016", "17:00", "18:00", "Indirect method in the calculus of variations. Derivative of a functional along a curve and in a given direction. First variation of a functional. Three basic examples of integral functionals depending only on the derivative."];
+Lezioni[3] = [ "3", "30/09/2016", "16:00", "17:00", "Fundamental lemma in the calculus of variations and Du Bois Reymond lemma: statements, proofs, discussion of possible variants. Example of minimum problem with an integral constraint."];
+Lezioni[4] = [ "4", "30/09/2016", "17:00", "18:00", "Genesis of boundary conditions for Euler equations. Examples of minimum problems originating Dirichlet, Neumann and periodic boundary conditions."];
+Lezioni[5] = [ "5", "05/10/2016", "14:00", "15:00", "Different forms of Euler-Lagrange equation: integral, differential, Du Bois Reymond and Erdmann form. Discussion of regularity issues in the derivation. Generalization to Lagrangians depending on higher order derivatives or more unknowns."];
+Lezioni[6] = [ "6", "05/10/2016", "15:00", "16:00", "Minimality through convexity. Minimality through auxiliary functional from below (trivial lemma). Examples of application in the case of Lagrangians depending only on the derivative."];
+Lezioni[7] = [ "7", "07/10/2016", "16:00", "17:00", "Point-to-curve problems. Transversality conditions."];
+Lezioni[8] = [ "8", "07/10/2016", "17:00", "18:00", "Final examples of application of the indirect method."];
+Lezioni[9] = [ "9", "12/10/2016", "14:00", "15:00", "Introduction to the direct method. Spaces with a notion of convergence and Weierstrass theorem for coercive and lower-semicontinuous functions. Separable Hilbert spaces: orthonormal systems and components."];
+Lezioni[10] = [ "10", "12/10/2016", "15:00", "16:00", "Strong convergence: continuity of the norm and lack of compactness of balls in infinite dimension. Weak convergence: definition, compactness of balls, lower semicontinuity of the norm."];
+Lezioni[11] = [ "11", "14/10/2016", "16:00", "17:00", "Passing to the limit in scalar products. Weakly convergent sequences are bounded. Weak convergence can be tested only on a set which spans a dense subspace."];
+Lezioni[12] = [ "12", "14/10/2016", "17:00", "18:00", "Summary of basic facts concerning Lebesgue measure and Lebesgue spaces. Fundamental lemma for measurable functions. Examples of weakly convergent sequences of functions."];
+Lezioni[13] = [ "13", "19/10/2016", "14:00", "15:00", "Sobolev spaces in dimension one: definition W vs definition H, basic properties and examples. Proof that H is contained in W."];
+Lezioni[14] = [ "14", "19/10/2016", "15:00", "16:00", "Antiderivative of weak derivatives. Proof that W is contained in H. Holder continuity of Sobolev functions. Weak derivatives and difference quotients."];
+Lezioni[15] = [ "15", "21/10/2016", "15:00", "16:00", "Road map of the direct method: weak formulation in Sobolev spaces, compactness of sub-levels with respect to a suitable notion of convergence, lower semicontinuity, regularity (initial step and bootstrap). First example of application."];
+Lezioni[16] = [ "16", "21/10/2016", "16:00", "17:00", "General discussion of compactness theorems in functional spaces. Passing to the limit in integral functionals (uniform convergence and continuous Lagrangian, weak convergence and convex Lagrangian)."];
+Lezioni[17] = [ "17", "26/10/2016", "14:00", "15:00", "Variational approach to a boundary value problem for a second order ordinary differential equation: existence, regularity, uniqueness under monotonicity assumptions. Discussion of different boundary conditions."];
+Lezioni[18] = [ "18", "26/10/2016", "15:00", "16:00", "Example of non-uniqueness for a Dirichlet problem. Variational approach to the existence of periodic solutions. Growth assumptions on the Lagrangian vs compactness of sub-levels."];
+Lezioni[19] = [ "19", "28/10/2016", "16:00", "17:00", "Definition of lower semicontinuous envelope and relaxation in metric spaces. Recovery sequences. The relaxation coincides with the lower semicontinuous envelope. Stability of the relaxation under continuous perturbations."];
+Lezioni[20] = [ "20", "28/10/2016", "17:00", "18:00", "Inf/min of a function vs inf/min of the relaxation. Minimizing sequences for a function vs recovery sequences for minimum points of the relaxation. Subsets dense in energy and their role in the computation of the relaxation."];
+Lezioni[21] = [ "21", "02/11/2016", "14:00", "15:00", "Strategies for computing a relaxation. Extension by relaxation and third characterization of Sobolev spaces. Example of relaxation of an integral functional."];
+Lezioni[22] = [ "22", "02/11/2016", "15:00", "16:00", "Definition of Gamma-convergence in metric spaces. Recovery sequences. Lack of connections with point-wise and uniform convergence. Connection with relaxation. Simple examples on the real line. Stability under continuous perturbations. Gamma-liminf and Gamma-limsup."];
+Lezioni[23] = [ "23", "04/11/2016", "14:00", "15:00", "Lower semicontinuity of Gamma-liminf and Gamma-limsup. Equicoerciveness. Convergence of minima and minimizers for equicoercive sequences."];
+Lezioni[24] = [ "24", "04/11/2016", "15:00", "16:00", "Proof of the Lagrange multipliers method in finite dimension by penalization of the constraint. Interpretation in terms of Gamma convergence. First example of study of the asymptotic behavior of a parametric minimum problems."];
+Lezioni[25] = [ "25", "09/11/2016", "14:00", "15:00", "Notions of minimum point: directional local minimum (DLM), weak local minimum (WLM), strong local minimum (SLM), global minimum (GM). Example of a WLM which is not a SLM. Second variation of a functional along a direction."];
+Lezioni[26] = [ "26", "09/11/2016", "15:00", "16:00", "Quadratic functionals. Legendre conditions. Jacobi differential equation, conjugate points and Jacobi conditions. Necessary conditions for a quadratic functional to be nonnegative."];
+Lezioni[27] = [ "27", "11/11/2016", "16:00", "17:00", "Sufficient conditions for a quadratic functional to be nonnegative. Oscillation lemma for second order linear ODEs."];
+Lezioni[28] = [ "28", "11/11/2016", "17:00", "18:00", "Strictly positive quadratic functionals. Estimates from below and from above for a quadratic functional. Necessary conditions for an extremal to be a DLM. Sufficient conditions for an extremal to be a WLM."];
+Lezioni[29] = [ "29", "16/11/2016", "14:00", "15:00", "Calibrations: introduction and motivating examples. Null Lagrangians and verification functions. Interpretation of minimality results for quadratic functionals and convex Lagrangians in terms of calibration through null Lagrangians. Level sets of verifications functions and calibration of curve-to-curve problems."];
+Lezioni[30] = [ "30", "16/11/2016", "15:00", "16:00", "Value function. A smooth value function is a verification function. Weierstrass excess function and Weierstrass conditions for SLM. Weierstrass fields and slope function."];
+Lezioni[31] = [ "31", "23/11/2016", "14:00", "15:00", "The existence of a Weierstrass field implies the Weierstrass representation formula: proof à la Hilbert (via null Lagrangian) and proof à la Weierstrass (in a special case)."];
+Lezioni[32] = [ "32", "23/11/2016", "15:00", "16:00", "Weierstrass necessary condition for an extremal to be a SLM. Idea of the proof of the imbedding theorem (Jacobi condition implies the existence of a Weierstrass field)."];
+Lezioni[33] = [ "33", "25/11/2016", "14:00", "15:00", "Lagrange multipliers in the calculus of variations: proof via implicit function theorem and via inverse function theorem. Example of application."];
+Lezioni[34] = [ "34", "25/11/2016", "15:00", "16:00", "First variation of functionals with multiple integrals: Dirichlet integral and Laplacian, Euler equation in divergence form, normal derivative at the boundary and Neumann boundary conditions."];
+Lezioni[35] = [ "35", "30/11/2016", "14:00", "15:00", "Definition of weak convergence in Lp. Weak lower semicontinuity under convexity assumptions. Weak compactness under super-linear growth assumptions. Weak convergence of bounded sequences can be tested on a dense subset."];
+Lezioni[36] = [ "36", "30/11/2016", "15:00", "16:00", "Regularity issues: lack of coerciveness of the Lagrangian and Holder regularity of the derivative of the minimizer. Examples of application of the direct method for Lagrangians with non-quadratic growth."];
+Lezioni[37] = [ "37", "02/12/2016", "14:00", "15:00", "Extension by relaxation of convex functionals to less regular ambient spaces. Density in energy of piecewise affine functions. Pathologies due to the lack of super-linear growth assumptions."];
+Lezioni[38] = [ "38", "02/12/2016", "15:00", "16:00", "Convexification of a function and its role in the computation of the relaxation of functionals with non-convex Lagrangian and suitable growth assumptions. Example of application of the theory developed so far."];
+Lezioni[39] = [ "39", "07/12/2016", "14:00", "15:00", "Classical examples: geodesics in the plane, on the cylinder and on the sphere (extremals, global/local minima, calibrations). Curves that minimize the Dirichlet integral are geodesics."];
+Lezioni[40] = [ "40", "07/12/2016", "15:00", "16:00", "Classical examples: obstacle problems. Direct method, with constraints on the function and/or on the derivative. Euler equation in form of inequality. Contact condition and optimal regularity in a contact point."];
+Lezioni[41] = [ "41", "09/12/2016", "16:00", "17:00", "Examples of Gamma-convergence: problems with small parameters (in the functional and/or in the boundary conditions) inducing linearization effects."];
+Lezioni[42] = [ "42", "09/12/2016", "17:00", "18:00", "Examples of Gamma-convergence: discrete-to-continuum models, from difference quotients to derivatives. Euler equation in the discrete setting."];
+Lezioni[43] = [ "43", "14/12/2016", "14:00", "15:00", "Classical problem: brachistocrone problem. Model, Euler equation, families of cycloids, existence/uniqueness of cycloid with given boundary conditions, global minimality (via Weierstrass field and via convexity trick)."];
+Lezioni[44] = [ "44", "14/12/2016", "15:00", "16:00", "Classical problem: cartesian Dido's problem. Euler equation with Lagrange multipliers, existence of solution depending on the parameter, global minimality, discussion of the case where no classical solution exists. Sufficient condition for minimality in a constrained minimization problem."];
+Lezioni[45] = [ "45", "16/12/2016", "16:00", "17:00", "Classical problem: minimal surface of revolution. Euler equation, existence/uniqueness depending on parameters, description of solutions in a special symmetric case, families of catenaries."];
+Lezioni[46] = [ "46", "16/12/2016", "17:00", "18:00", "Classical problem: heavy chain. Cartesian and parametric formulation, Euler equation with Lagrange multipliers, existence/uniqueness depending on the parameters. General discussion of minimum problems with point-wise constraint on derivatives: relaxation and saturation of the constraint."];
+Lezioni[47] = [ "47", "21/12/2016", "14:00", "15:00", "Examples of Gamma-convergence: homogenization problems with oscillating coefficients either on the function or on the derivative. Cell problem."];
+Lezioni[48] = [ "48", "21/12/2016", "15:00", "16:00", "Examples of Gamma-convergence: Modica-Mortola functional in dimension one (asymptotic study of the minimum value and the optimal transition profile)."];
---
+++ js/corsi/cdv-2017-18.js
@@ -0,0 +1,87 @@
+var NomeCorso = "Elementi di Calcolo delle Variazioni - 2017/2018";
+var Percorso = MEDIA.lezioni + "CdV_18/CdV_18_L";
+var CifreNumeroLezione = 2;
+
+var SchedaCorso = {
+  nome: "Elementi di Calcolo delle Variazioni",
+  annoAccademico: "2017/2018",
+  crediti: "6",
+  ore: "48",
+  target: "Studenti di Matematica (ma ognuno è benvenuto, ovviamente)",
+  anno: "Third year students or more",
+  periodo: "End of September / December",
+  avviso: "Questo corso è la terza versione del corso tenuto per la prima volta nel 2015/16. Si tratta di un’introduzione di base ai funzionali integrali scalari, con meno prerequisiti possibile."
+};
+
+var DocumentiCorso = [
+  {
+    sezione: "burocrazia",
+    file: "CdV_18_Burocrazia.pdf",
+    titolo: "Informazioni generali"
+  },
+  {
+    sezione: "burocrazia",
+    file: "CdV_18_Esame.pdf",
+    titolo: "Regole d'esame"
+  },
+  {
+    sezione: "burocrazia",
+    file: "CdV_18_Programma.pdf",
+    titolo: "Programma dettagliato"
+  },
+  {
+    sezione: "book",
+    file: "CdV_18_Book.pdf",
+    titolo: "Stampato integrale lezioni"
+  }
+];
+
+var Lezioni = new Array();
+Lezioni[1] = [ "1", "26/09/2017", "14:00", "15:00", "Presentazione degli argomenti del corso mediante semplici esempi. Funzionali integrali, con esempi. Esempi di problemi che non verranno trattati."];
+Lezioni[2] = [ "2", "26/09/2017", "15:00", "16:00", "Introduzione al metodo indiretto nel calcolo delle variazioni. Variazione prima di un funzionale lungo una curva e in una direzione. Forme integrali e differenziali della variazione prima. Studio esplicito di tre casi modello di funzionali integrali dipendenti da potenze della derivata."];
+Lezioni[3] = [ "3", "29/09/2017", "16:00", "17:00", "Lemma fondamentale del calcolo delle variazioni e lemma di Du Bois Reymond: enunciato, possibili dimostrazioni, discussione di possibili varianti. Esempio di un problema di minimo con vincolo integrale."];
+Lezioni[4] = [ "4", "29/09/2017", "17:00", "18:00", "Nascita delle condizioni al bordo per le equazioni di Eulero-Lagrange. Esempi che portano a condizioni di Dirichlet, di Neumann, e periodiche. Esempi di problemi in cui il minimo non esiste."];
+Lezioni[5] = [ "5", "03/10/2017", "14:00", "15:00", "Equazione di Eulero-Lagrange per una Lagrangiana generale: forme integrali, forma differenziale classica, alla Du Bois Reymond, alla Erdmann per il caso autonomo. Condizioni di Neumann per Lagrangiane generali. Discussione delle ipotesi di regolarità necessarie per le varie forme."];
+Lezioni[6] = [ "6", "03/10/2017", "15:00", "16:00", "Come dimostrare che un punto stazionario è un punto di minimo: utilizzo della convessità o di un opportuno funzionale ausiliario. Esempi di esistenza e non esistenza del minimo per una Lagrangiana non convessa dipendente dalla sola derivata."];
+Lezioni[7] = [ "7", "10/10/2017", "14:00", "15:00", "Problemi point-to-curve. Condizioni di transversality."];
+Lezioni[8] = [ "8", "10/10/2017", "15:00", "16:00", "Discussione sullo spazio in cui ambientare i problemi di minimo. Esempi finali di metodo indiretto (con argomenti di troncamento e perdita di regolarità)."];
+Lezioni[9] = [ "9", "12/10/2017", "14:00", "15:00", "Introduzione al metodo diretto. Spazi con una nozione di convergenza e teorema di Weierstrass per funzioni semicontinue inferiormente e coercive. Spazi di Hilbert separabili: sistemi ortonormali (basi Hilbertiane) e componenti."];
+Lezioni[10] = [ "10", "12/10/2017", "15:00", "16:00", "Convergenza forte: continuità della norma e non compattezza delle palle in dimensione infinita. Convergenza debole: definizione, prime proprietà, semicontinuità della norma, compattezza delle palle."];
+Lezioni[11] = [ "11", "17/10/2017", "14:00", "15:00", "Passaggio al limite nei prodotti scalari. La convergenza debole di successioni limitate può essere testata limitandosi ad un sottoinsieme con span denso. Le successioni debolmente convergenti sono limitate."];
+Lezioni[12] = [ "12", "17/10/2017", "15:00", "16:00", "Ricapitolazione di fatti noti di analisi funzionale: teoremi di passaggio al limite per integrali, spazi di Lebesgue, disuguaglianze alla Holder. Lemma fondamentale del calcolo delle variazioni negli spazi di Lebesgue. Esempio di successione di funzioni debolmente convergente."];
+Lezioni[13] = [ "13", "20/10/2017", "14:00", "15:00", "Spazi di Sobolev in dimensione uno: definizione W (mediante integrazione per parti) vs definizione H (mediante approssimazione o completamento astratto). Principali proprietà ed esempi. Dimostrazione che H è contenuto in W."];
+Lezioni[14] = [ "14", "20/10/2017", "15:00", "16:00", "Le funzioni di Sobolev sono le primitive delle loro derivate deboli. Dimostrazione che W è contenuto in H. Le funzioni di Sobolev sono Holderiane. Derivate deboli e rapporti incrementali."];
+Lezioni[15] = [ "15", "23/10/2017", "16:00", "17:00", "Road map del metodo diretto: formulazione debole negli spazi di Sobolev, compattezza dei sottolivelli rispetto ad un'opportuna nozione di convergenza, semicontinuità, recupero della regolarità (passo iniziale e bootstrap). Primo esempio di applicazione."];
+Lezioni[16] = [ "16", "23/10/2017", "17:00", "18:00", "Discussione generale di come ottenere compattezza in spazi funzionali, sotto ipotesi di crescita per la Lagrangiana. Passaggio al limite nei funzionali integrali (caso di uniforme convergenza e Lagrangiana continua, caso di convergenza debole e Lagrangiana convessa)."];
+Lezioni[17] = [ "17", "24/10/2017", "14:00", "15:00", "Approccio variazionale ai problemi al bordo per equazioni differenziali di ordine due: esistenza, regolarità, unicità sotto ipotesi di monotonia. Discussione di diverse condizioni al bordo di Dirichlet/Neumann."];
+Lezioni[18] = [ "18", "24/10/2017", "15:00", "16:00", "Esempio di non unicità per un problema di Dirichlet. Approccio variazionale all'esistenza di soluzioni periodiche. Regolarità e stretta positività della derivata seconda della Lagrangiana rispetto alla velocità."];
+Lezioni[19] = [ "19", "28/10/2017", "16:00", "17:00", "Definizione di inviluppo semicontinuo e rilassamento in spazi metrici. Principali proprietà del rilassamento: lemma fondamentale, semicontinuità inferiore, l'inf nella definizione è un minimo, coincide con l'inviluppo semicontinuo. Recovery sequences."];
+Lezioni[20] = [ "20", "28/10/2017", "17:00", "18:00", "Inf/min di una funzione vs inf/min del rilassato (in generale e sotto ipotesi di coercività). Successioni minimizzanti per una funzione vs recovery sequences dei punti di minimo del rilassato. Prima strategia per il calcolo di un rilassato. Stabilità del rilassato rispetto a perturbazioni continue."];
+Lezioni[21] = [ "21", "30/10/2017", "16:00", "17:00", "Sottoinsiemi densi in energia e relativo lemma. Seconda strategia per il calcolo di un rilassato. Estensione per rilassamento (ed ulteriore caratterizzazione degli spazi di Sobolev). Esempio di calcolo del rilassato di un funzionale integrale."];
+Lezioni[22] = [ "22", "30/10/2017", "17:00", "18:00", "Definizione di Gamma-convergenza in spazi metrici. Recovery sequences. Gamma-convergenza e rilassamento. Rapporti (quasi inesistenti) con la convergenza puntuale ed uniforme. Esempi sulla retta reale. Stabilità rispetto a perturbazioni continue. Definizione di Gamma-liminf e Gamma-limsup."];
+Lezioni[23] = [ "23", "31/10/2017", "14:00", "15:00", "Proprietà del Gamma-liminf: lemma fondamentale, l'inf nella definizione è un minimo, semicontinuità inferiore, convergenza dei minimi (caso degli aperti e dei compatti). Definizione di successione equicoerciva."];
+Lezioni[24] = [ "24", "31/10/2017", "15:00", "16:00", "Teorema di convergenza dei minimi e dei punti di minimo (sotto ipotesi di Gamma-convergenza ed equicoercività). Proprietà del Gamma-limsup: lemma fondamentale, l'inf nella definizione è un minimo. Primo esempio di studio asintotico di un problema di minimo dipendente da un parametro."];
+Lezioni[25] = [ "25", "03/11/2017", "16:00", "17:00", "Diverse tipologie di punto di minimo: globale (GM), locale forte (SLM), locale debole (WLM), direzionale (DLM). Esempio di WLM che non è SLM. Variazione seconda di un funzionale lungo una direzione e sua espressione in termini della Lagrangiana."];
+Lezioni[26] = [ "26", "03/11/2017", "17:00", "18:00", "Funzionali quadratici. Condizioni di Legendre. Equazione di Jacobi e definizione di punto coniugato. Condizioni di Jacobi. Condizioni necessarie affinché un funzionale quadratico sia non negativo."];
+Lezioni[27] = [ "27", "07/11/2017", "14:00", "15:00", "Condizioni sufficienti affinché un funzionale quadratico sia non negativo. Legami tra equazioni di Riccati e soluzioni positive di equazioni lineari di ordine due. Dimostrazione del lemma di oscillazione."];
+Lezioni[28] = [ "28", "07/11/2017", "15:00", "16:00", "Funzionali quadratici strettamente positivi. Stime dall'alto e dal basso per funzionali quadratici. Condizioni necessarie per essere DLM. Condizioni sufficienti per essere WLM."];
+Lezioni[29] = [ "29", "14/11/2017", "14:00", "15:00", "Calibrazioni: esempi motivazionali. Null Lagrangians e verification functions. Interpretazione dei risultati di minimalità per funzionali quadratici e funzionali convessi in termini di calibrazioni mediante null Lagrangians. Insiemi di livello delle verifications functions e calibrazione di problemi curve-to-curve."];
+Lezioni[30] = [ "30", "14/11/2017", "15:00", "16:00", "Value function. Una value function regolare è una verification function. Eccesso di Weierstrass e condizioni di Weierstrass per essere SLM. Definizione di campo di Weierstrass."];
+Lezioni[31] = [ "31", "17/11/2017", "16:00", "17:00", "Slope function di un campo di Weierstrass. L'esistenza di un campo di Weierstrass implica la formula di rappresentazione di Weierstrass: dimostrazione alla Hilbert (via null Lagrangian) e dimostrazione alla Weierstrass (per campi con un punto base)."];
+Lezioni[32] = [ "32", "17/11/2017", "17:00", "18:00", "Dimostrazione della condizione necessaria di Weierstrass per uno SLM. Dimostrazione della condizione sufficiente a partire dalla formula di rappresentazione. Idea della dimostrazione dell'embedding theorem (dall'equazione di Jacobi all'esistenza di campi di Weierstrass)."];
+Lezioni[33] = [ "33", "21/11/2017", "14:00", "15:00", "Metodo dei moltiplicatori di Lagrange nel calcolo delle variazioni: dimostrazione via teorema della funzione implicita e via teorema della funzione inversa. Condizione sufficiente per essere punto di minimo in un problema vincolato. Esempio di applicazione del metodo."];
+Lezioni[34] = [ "34", "21/11/2017", "15:00", "16:00", "Variazione prima di funzionali con integrali multipli: integrale di Dirichlet e Laplaciano, equazione di Eulero-Lagrange in forma di divergenza, derivata normale al bordo e condizioni al bordo di Neumann."];
+Lezioni[35] = [ "35", "24/11/2017", "14:00", "15:00", "Definizione di convergenza debole in Lp. Semicontinuità rispetto alla convergenza debole sotto ipotesi di convessità. Per le successioni limitate è possibile limitarsi a testare la convergenza debole su un denso."];
+Lezioni[36] = [ "36", "24/11/2017", "15:00", "16:00", "Compattezza debole sotto ipotesi di crescita superlineare. Questioni di regolarità: mancanza di coercività vs regolarità Holderiana della derivata dei minimi. Esempio di metodo diretto per una Lagrangiana con dipendenza accoppiata da funzione/derivata"];
+Lezioni[37] = [ "37", "27/11/2017", "14:00", "15:00", "Estensione per rilassamento di funzionali convessi da ambienti regolari ad ambienti meno regolari. Densità in energia delle funzioni affini a tratti. Patologie dovute alla mancanza di crescita superlineare."];
+Lezioni[38] = [ "38", "27/11/2017", "15:00", "16:00", "Inviluppo convesso di una funzione. Rilassamento di funzionali integrali con Lagrangiana non convessa (ma con ipotesi di crescita). Esempi di applicazione della teoria."];
+Lezioni[39] = [ "39", "28/11/2017", "14:00", "15:00", "Esempi classici: geodetiche in spazi euclidei, sul cilindo e sulla sfera (estremali, minimi locali/globali, calibrazioni). Le curve che minimizzano l'integrale di Dirichlet sono geodetiche."];
+Lezioni[40] = [ "40", "28/11/2017", "15:00", "16:00", "Esempi classici: problemi con ostacolo. Metodo diretto, sia con ostacolo sulla funzione, sia con ostacolo sulla derivata. Equazione di Eulero-Lagrange in forma di disuguaglianza. Condizioni di contatto e regolarità ottimale in un punto di contatto."];
+Lezioni[41] = [ "41", "04/12/2017", "16:00", "17:00", "Esempi di Gamma-convergenza: problemi con parametri piccoli (nel funzionale o nelle condizioni al bordo) che inducono effetti di linearizzazione."];
+Lezioni[42] = [ "42", "04/12/2017", "17:00", "18:00", "Esempi di Gamma-convergenza: problemi con passaggio dal discreto al continuo, dai rapporti incrementali alle derivate. Integrazione per parti discreta. Equazione di Eulero nel contesto discreto."];
+Lezioni[43] = [ "43", "05/12/2017", "14:00", "15:00", "Problema classico: la brachistocrona. Modello fisico, equazione di Eulero, famiglie di cicloidi, esistenza ed unicità della cicloide che rispetta le condizioni al bordo, effettiva minimalità globale (via campi di Weierstrass e via trucco di convessità)."];
+Lezioni[44] = [ "44", "05/12/2017", "15:00", "16:00", "Problema classico: problema di Didone in versione cartesiana. Equazione di Eulero con moltiplicatore di Lagrange, esistenza di soluzioni al variare del parametro, minimalità globale (via campi di Weierstrass e via vera convessità). Discussione del caso in cui una soluzione classica non esiste."];
+Lezioni[45] = [ "45", "12/12/2017", "14:00", "15:00", "Problema classico: superfici di rotazione di area minima. Equazione di Eulero, discussione di esistenza/unicità a seconda dei parametri, famiglie di catenarie, discussione delle soluzioni nel caso simmetrico."];
+Lezioni[46] = [ "46", "12/12/2017", "15:00", "16:00", "Problema classico: heavy chain. Formulazione cartesiana, equazione di Eulero con moltiplicatori di Lagrange, esistenza/unicità a seconda dei parametri. Discussione generale di problemi di minimo con vincoli integrali/puntuali sulle derivate: rilassamento (da uguaglianza a disuguaglianza) e saturazione del vincolo."];
+Lezioni[47] = [ "47", "15/12/2017", "16:00", "17:00", "Esempi di Gamma-convergenza: problemi di omogenizzazione con coefficienti oscillanti, sia davanti alla funzione, sia davanti alla derivata. Problema di cella."];
+Lezioni[48] = [ "48", "15/12/2017", "17:00", "18:00", "Esempi di Gamma-convergenza: funzionale di Modica-Mortola in dimensione uno (studio asintotico del valore del minimo e del profilo di transizione ottimale)."];
