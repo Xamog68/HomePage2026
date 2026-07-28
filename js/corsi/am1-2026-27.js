a522bf366de433c5742afb1bef1e612c9895cacf
Massimo Gobbino <massimo.gobbino@unipi.it>
2026-07-28 12:25:31 +0200

    Aggiunge il corso di Analisi 1 2026/27

---
+++ js/corsi/am1-2026-27.js
@@ -0,0 +1,19 @@
+var NomeCorso = "Analisi Matematica 1 - 2026/2027";
+var Percorso = MEDIA.lezioni + "AM1_27/AM1_27_L";
+
+var SchedaCorso = {
+  nome: "Analisi Matematica 1",
+  annoAccademico: "2026/2027",
+  crediti: "15",
+  ore: "120",
+  target: "Matematica",
+  anno: "Primo",
+  periodo: "fine settembre / fine maggio",
+  avviso:
+    "Le lezioni e i materiali saranno pubblicati progressivamente " +
+    "durante l’anno accademico."
+};
+
+var DocumentiCorso = [];
+
+var Lezioni = new Array();
