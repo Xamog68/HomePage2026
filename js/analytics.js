b4a59659388b5ab8531ca8c109cf255bceacef40
Massimo Gobbino <massimo.gobbino@unipi.it>
2026-07-31 12:26:46 +0200

    Semplifica inizializzazione Google Analytics

--- js/analytics.js
+++ js/analytics.js
@@ -4,28 +4,35 @@ if (
   window.location.hostname !== "localhost" &&
   window.location.hostname !== "127.0.0.1"
 ) {
-  window.dataLayer = window.dataLayer || [];
-
-  function gtag() {
-    dataLayer.push(arguments);
-  }
-
-  window.gtag = gtag;
-
-  gtag("js", new Date());
-  gtag("config", GA_MEASUREMENT_ID, {
-    cookie_domain: "none",
-    debug_mode: true
-  });
-
+  /*
+    Carica per prima la libreria ufficiale di Google Analytics.
+  */
   const scriptAnalytics = document.createElement("script");
   scriptAnalytics.async = true;
   scriptAnalytics.src =
     "https://www.googletagmanager.com/gtag/js?id=" +
-    GA_MEASUREMENT_ID;
+    encodeURIComponent(GA_MEASUREMENT_ID);
 
   document.head.appendChild(scriptAnalytics);
+
+  /*
+    Inizializza la coda usata da gtag.js.
+  */
+  window.dataLayer = window.dataLayer || [];
+
+  window.gtag = function () {
+    window.dataLayer.push(arguments);
+  };
+
+  window.gtag("js", new Date());
+
+  window.gtag("config", GA_MEASUREMENT_ID, {
+    debug_mode: true
+  });
 } else {
+  /*
+    In locale non invia dati, ma mostra gli eventi in console.
+  */
   window.gtag = function () {
     console.log("Analytics locale:", ...arguments);
   };
